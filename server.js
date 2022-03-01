cc5009819cd46d90df7c46e8d888384c75b798cc
Spotexx <tsanderson.023@gmail.com>
2022-03-01 11:15:33 -0500

    boiler plate code

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./routes');
+const exphbs = require('express-handlebars');
+
+
+const session = require('express-session')
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
